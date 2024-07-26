24f7403bd27345b5ad1b264390e08cb3c964a300
Youwen Wu <youwenw@gmail.com>
2024-07-25 22:00:46 -0700

    some updates

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Flake for a NixOS Mac Mini server.";
+
+  inputs = {
+    # NixOS official package source, using the nixos-23.11 branch here
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # Please replace my-nixos with your hostname
+    nixosConfigurations.gallium = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        # Import the previous configuration.nix we used,
+        # so the old configuration file still takes effect
+        ./configuration.nix
+      ];
+    };
+  };
+}
